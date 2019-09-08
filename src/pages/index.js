efcad8ec9be90f702269a95f6299457f8f7b0037
Ali Briceño <alienriquebm@gmail.com>
2019-09-08 16:00:25 -0400

    added sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+import './index.scss';
 
-export default () => <div>Hello world!</div>
+export default () => <div className="Main">
+  <Button type="primary">Primary</Button>
+  <Button>Default</Button>
+  <Button type="dashed">Dashed</Button>
+  <Button type="danger">Danger</Button>
+  <Button type="link">Link</Button>
+</div>
